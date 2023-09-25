3ce0bd6ff4ce416b03b114a91beff31334e6965b
Sudakshin <sudakshin9@gmail.com>
2023-09-25 22:56:19 +0530

    till events lecture

--- src/index.js
+++ src/index.js
@@ -3,37 +3,43 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const BookList = () => {
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "./images/img.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
+
+function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        console.log(book);
+        const { img, title, author, id } = book;
+        return <Book {...book} key={id} />;
+      })}
     </section>
   );
-};
+}
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
     </article>
   );
-};
-
-const Image = () => (
-  //   <img
-  //     src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-  //     alt="Atomic Habits Cover Image"
-  //   />
-  <img src="./img.jpg" alt="Atomic Habits Cover Image" />
-);
-const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => {
-  return <h4>James Clear</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
